ecf08322a235c7c4c9246dfe25fc9623a9edb7b5
Gonzalo Bulnes Menjibar <gonsevi80@gmail.com>
2024-02-06 16:21:33 +0100

    arreglado editar noticia

--- src/middleware/authUserController.js
+++ src/middleware/authUserController.js
@@ -1,30 +1,31 @@
-import jwt from 'jsonwebtoken';
-import { invalidCredentialsError, notAuthenticatedError } from '../services/errorService.js';
+import jwt from "jsonwebtoken";
+import {
+  invalidCredentialsError,
+  notAuthenticatedError,
+} from "../services/errorService.js";
 
+const authUserController = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
 
-const authUserController = (req,res,next) => {
-    try {
-        const { authorization } = req.headers;
-
-        if(!authorization){
-            notAuthenticatedError();
-        }
-
-        let tokenInfo;
-
-        try {
-            tokenInfo = jwt.verify(authorization, process.env.SECRET);
-        } catch (error) {
-            invalidCredentialsError();
-        }
-
-        req.user = tokenInfo;
-
-        next();
-    } catch (error) {
-        next(error);
+    if (!authorization) {
+      notAuthenticatedError();
     }
-}
 
+    let tokenInfo;
+
+    try {
+      tokenInfo = jwt.verify(authorization, process.env.SECRET);
+    } catch (error) {
+      invalidCredentialsError();
+    }
+
+    req.user = tokenInfo;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default authUserController;
